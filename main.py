7658052d7a304acca53840f52439ce0566010535
Clément <clement.savinaud@mines-ales.org>
2021-06-16 12:05:06 +0200

    Ajout du calcul de la distance et du parcours de l'arbre des possibilités

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from coordinates import *
 
 global towns
-global MINIMUMCHEMIN
+MINIMUM_DISTANCE = -1
+MINIMUM_CHEMIN = []
+
+NB_CHEMIN = 0
+
+def travelTown(previousTown, availableTown):
+    global MINIMUM_CHEMIN
+    global MINIMUM_DISTANCE
+    global NB_CHEMIN
+    if len(availableTown) > 0:
+        for town in availableTown:
+            previousTown.append(town)
+            availableTown.remove(town)
+            travelTown(previousTown.copy(), availableTown.copy())
+            print(previousTown)
+            print(availableTown)
+
+            
+    else:
+        NB_CHEMIN += 1
+        distance = getFullDistance(previousTown)
+        if MINIMUM_DISTANCE == -1 or MINIMUM_DISTANCE > distance:
+            MINIMUM_DISTANCE = distance
+            MINIMUM_CHEMIN = previousTown.copy()
+
+def getFullDistance(towns):
+    distance = 0
+    previousTown = towns[0]
+    for town in towns[1:]:
+        distance += townsWithDistance[previousTown][town]
+        previousTown = town
+    return distance
 
 dict = getTowns('tsp1.txt')
-towns = setDistances(dict)
+townsWithDistance = setDistances(dict)
 
+travelTown([], list(townsWithDistance.keys()))
 
-def plusCourtChemin():
-    town = towns[]
+print('Nombre de chemins calculés : {}'.format(NB_CHEMIN))
+print('Distance minimale trouvée : {}'.format(MINIMUM_DISTANCE))
+print('Chemin minimal : {}'.format(MINIMUM_CHEMIN))
